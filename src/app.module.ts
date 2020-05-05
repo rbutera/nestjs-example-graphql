d1e1ddbb1d90b7f3268cdc5a074582fdaa273f1d
Rai Butera <rai@rbutera.com>
2020-05-05 18:21:49 +0100

    generated lesson module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import {Module} from '@nestjs/common'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
 import {GraphQLModule} from '@nestjs/graphql'
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
+    LessonModule,
   ],
   controllers: [AppController],
   providers: [AppService],
