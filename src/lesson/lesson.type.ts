8c34bce5fd193114714d08b9d7c0589feb8fcd85
Rai Butera <rai@rbutera.com>
2020-05-05 18:24:00 +0100

    fix graphql type

--- src/lesson/lesson.type.ts
+++ src/lesson/lesson.type.ts
@@ -1,8 +1,17 @@
-// Lesson graphql type
+import {ObjectType, Field} from '@nestjs/graphql'
 
+// Lesson graphql type
+@ObjectType()
 export class LessonType {
+  @Field()
   id: string
+
+  @Field()
   name: string
+
+  @Field()
   startDate: string // ISO strings
+
+  @Field()
   endDate: string // ISO strings
 }
