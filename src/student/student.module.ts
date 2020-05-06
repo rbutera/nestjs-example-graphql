388769a9cdecae3bb9cb2ae5fe3f5a5235279cf8
Rai Butera <rai@rbutera.com>
2020-05-06 19:20:09 +0100

    inject TypeOrmModule into student module

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common';
-import { StudentService } from './student.service';
+import {Student} from './student.entity'
+import {TypeOrmModule} from '@nestjs/typeorm'
+import {Module} from '@nestjs/common'
+import {StudentService} from './student.service'
 
 @Module({
-  providers: [StudentService]
+  imports: [TypeOrmModule.forFeature([Student])],
+  providers: [StudentService],
 })
 export class StudentModule {}
