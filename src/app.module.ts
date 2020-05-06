2eac7910461c0135de5c3a179ba8d1ebbd688c51
Rai Butera <rai@rbutera.com>
2020-05-06 12:00:41 +0100

    add and configure mongodb and orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,23 @@ import {Module} from '@nestjs/common'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
 import {GraphQLModule} from '@nestjs/graphql'
-import { LessonModule } from './lesson/lesson.module';
+import {LessonModule} from './lesson/lesson.module'
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: '',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [],
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     LessonModule,
+    TypeOrmModule,
   ],
   controllers: [AppController],
   providers: [AppService],
